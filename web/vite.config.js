fea900ff9a383b605f916c5dcc8902f81a31581a
daisy huang <daisyhuang1993@gmail.com>
2026-03-24 23:20:02 -0400

    vite preview allowed list

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['capyai.daisyhuang.dev'],
+  },
   server: {
     host: true,
     port: 5173,
